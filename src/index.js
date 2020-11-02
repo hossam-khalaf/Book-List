530423f176dcb59511bec56c8024230bbcd95ec7
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-11-02 18:37:48 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// statless functional component
+// always return JSX
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Person = () => <h2>Hossam Hassan</h2>;
+const Message = () => {
+  return <p>This is my message</p>;
+};
+
+// const Greeting = () => {
+//   return React.createElement('h1', {}, 'Hello world!');
+// };
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
