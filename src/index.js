e71e82623639fccba17b99c3afeeb3dd5cdc9b14
hossam-khalaf <hossam.khalaf@med.au.edu.eg>
2020-11-02 21:23:11 +0200

    props!

--- src/index.js
+++ src/index.js
@@ -4,25 +4,47 @@ import ReactDom from 'react-dom';
 //css
 import './index.css';
 
+// setup variables
+// A Promised Land
+const firstBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg',
+  title: 'Greenlights',
+  author: 'Matthew McConaughey',
+};
+const secondBook = {
+  img:
+    'https://images-na.ssl-images-amazon.com/images/I/91uwocAMtSL._AC_UL200_SR200,200_.jpg',
+  title: 'A Promised Land',
+  author: 'Barack Obama',
+};
+
 // statless functional component
 // always return JSX
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/81Kc8OsbDxL._AC_UL200_SR200,200_.jpg'
-        alt=''
-      />
-      <h3>Greenlights</h3>
-      <h4>Matthew McConaughey</h4>
+      <img src={props.img} alt='' />
+      <h3>{props.title}</h3>
+      <h4>{props.author}</h4>
     </article>
   );
 };
